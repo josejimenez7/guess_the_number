cf034646e846dee8fb6d3100d2ccfcdf657d401e
josejimenez7 <jbjimenez77@gmail.com>
2014-03-19 18:05:30 +0000

    change if statement to until block

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,3 +1,12 @@
 p "welcome to guess the number"
-secret_numebr = rand(6)
-puts secret_number
+secret_number = rand(99)
+
+p "Guess a number between 0 and 100:"
+guess = gets.chomp.to_i
+
+until guess == secret_number
+    p "worng!, try again!"
+    guess = gets.chomp.to_i
+end
+
+p "congrats! you won!"
