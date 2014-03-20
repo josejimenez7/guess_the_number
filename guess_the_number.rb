d61fffacdce068edfcd023f9f71fc0745db0fafa
josejimenez7 <jbjimenez77@gmail.com>
2014-03-20 17:55:49 +0000

    change thr game from numbers to letters

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,12 +1,13 @@
-p "welcome to guess the number"
-secret_number = rand(99)
+p "welcome to guess the letter"
+letters_alphabet =[ "A" , "a", "B" ,"b", "C", "c", "D", "e", "E", "f", "F","g", "G","h", "H", "i", "I", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z"]
+guess_the_letter
 
-p "Guess a number between 0 and 100:"
+p "Guess a diffrent letter form the alphabet!"
 guess = gets.chomp.to_i
 
-until guess == secret_number
+until guess == secret_letter
     p "worng!, try again!"
     guess = gets.chomp.to_i
 end
 
-p "congrats! you won!"
+p "congrats! you won"
